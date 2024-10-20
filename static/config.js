9512438a0afa4a594a8771fc16a17b0296f541b3
strugglebright <601632305@qq.com>
2024-10-20 17:09:40 +0800

    初始化博客

---
+++ static/config.js
@@ -0,0 +1,12 @@
+/**
+ * 全局配置
+ */
+var CONFIG = {
+<<<<<<< HEAD
+  baseUrl: 'https://fc-mp-a8fcc9bb-1ab1-49d9-9618-0d4b1c300e0c.next.bspapp.com',  // 请求地址  fc开头是指URL化的云函数地址 
+=======
+  baseUrl: 'https://fc-mp-a8fcc9bb-1ab1-49d9-9618-0d4b1c300e0c.next.bspapp.com',  // 请求地址
+>>>>>>> ab2c5186cfb4e4435bfeb3582c2a7423da86d726
+  author: 'Bright', 
+  version: '1.0.0'  // 版本
+};
