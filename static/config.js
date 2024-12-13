de3b8084b9ccce793459b099caa8c689737f9945
Bright2017 <www.601632305@qq.com>
2024-12-13 10:47:48 +0800

    更新图片

--- static/config.js
+++ static/config.js
@@ -4,5 +4,7 @@
 var CONFIG = {
   baseUrl: 'https://fc-mp-a8fcc9bb-1ab1-49d9-9618-0d4b1c300e0c.next.bspapp.com',  // 请求地址  fc开头是指URL化的云函数地址 
   author: 'Bright', 
-  version: '1.0.0'  // 版本
+  version: '1.0.0',  // 版本
+  isRequest: false,  //  不调接口 用静态数据
+  isProduction: true  // true 图片使用托管地址 false 使用静态图片
 };
